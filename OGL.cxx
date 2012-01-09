b12b507e28dc290ba3ec091affcd00bcc946f179
Yann Battista <yann.batt@gmail.com>
2012-01-09 15:38:03 +0100

    Merge

--- OGL.cxx
+++ OGL.cxx
@@ -18,7 +18,7 @@ void affichage ()
 	glColor3f(1.0, 1.0, 1.0);
 
 	glBegin(GL_QUADS);
-	glVertex3f(0.0, 0.0, 0.0);
+	glVertex3f(1.0, 0.0, 1.0);
 	//glColor3f(1.0, 1.0, 0.0);
 	glVertex3f(1.0, 0.0, 0.0);
 	//glColor3f(0.0, 1.0, 1.0);
