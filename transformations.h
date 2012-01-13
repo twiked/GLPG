e526e4789ec1933f645c26dcc161e7a002eb62d3
Eric G. <fear_hardcore@yahoo.fr>
2012-01-13 12:20:14 +0100

    Camera fixed, même FOV partout. Maintenant il faut implémenter les mouvements de cube associés à la souris, ainsi que la camera au clavier

--- transformations.h
+++ transformations.h
@@ -1,3 +1,7 @@
+#include <cstring>
+
+#define NATIVE true
+
 GLfloat * CreateMatEch (GLfloat x, GLfloat y, GLfloat z)
 {
 	GLfloat M[] =
@@ -28,14 +32,14 @@ GLfloat* CreateMatRotz (GLfloat angle)
 	return M;
 };
 
-GLfloat* CreateMatRoty (GLfloat angle)
+void CreateMatRoty (GLfloat* M, GLfloat angle)
 {
-	GLfloat M[] =
+	GLfloat L[] =
 { cosf(angle),  0.0, -sinf(angle), 0.0,
   0.0,          1.0, 0.0,          0.0,
   sinf(angle),  0.0, cosf(angle),  0.0,
   0.0,          0.0, 0.0,          1.0};
-	return M;
+	memcpy( M , L , sizeof(GLfloat)*16);
 };
 
 GLfloat* CreateMatTransVec (GLfloat Tx, GLfloat Ty, GLfloat Tz)
@@ -48,14 +52,15 @@ GLfloat* CreateMatTransVec (GLfloat Tx, GLfloat Ty, GLfloat Tz)
 	return M;
 }
 
-GLfloat* CreateMatRotAxez (GLfloat angle)
+void CreateMatRotAxez (GLfloat* M, GLfloat angle)
 {
-	GLfloat M[] =
-{ (3 + cos (angle))/4,  				(sqrtf(3) * (-cos (angle) + 1))/4, 	(-2 * sin (angle))/4, 			0.0,
-  (sqrtf(3) * (-cos (angle) + 1))/4, 	(3 * cos(angle) +1)/4, 				(2* sqrtf(3) * sin(angle))/4, 	0.0,
-  (2 * sin (angle))/4, 					(-2 * sqrtf(3) * sin(angle))/4, 	(4 * cos(angle))/4, 			0.0,
-  0.0,  								0.0,  								0.0,  							1.0};
-	return M;
+	GLfloat L[] =
+{ (3 + cos (angle)),  				(sqrtf(3) * (-cos (angle) + 1)), 	(-2 * sin (angle)), 			0.0,
+  (sqrtf(3) * (-cos (angle) + 1)), 	(3 * cos(angle) +1), 				(2* sqrtf(3) * sin(angle)), 	0.0,
+  (2 * sin (angle)), 					(-2 * sqrtf(3) * sin(angle)), 	(4 * cos(angle)), 			0.0,
+  0.0,  								0.0,  								0.0,  							4.0};
+	memcpy( M , L , sizeof(GLfloat)*16);
+	
 }
 
 /*GLfloat* CreateMatTransAngz (GLfloat angle, )
