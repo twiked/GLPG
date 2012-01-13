ee82407f64039035983d3f00fa14ac1f69f5fa56
Eric Gillet <fear_hardcore@yahoo.fr>
2012-01-13 01:22:52 +0100

    Corrigé le problème de transparence du cube, ajouté la perspective (FOV) ! attention il faut compiler avec -lGLU et peut être également -lGL ! Remplacé les fonctions de création des matrices par les appels natifs OpenGL (il faut quand même les corriger) et séparé en deux fichiers le projet

--- OGL.cxx
+++ OGL.cxx
@@ -3,97 +3,79 @@
 #include <iostream>
 #include <sstream>
 
-#define PI 3.1415926535897932
+#include "transformations.h"
 
+#define PI 3.1415926535897932
 namespace
 {
-	int x=0, y=0, w=250, h = 250;
+	int x=0, y=0, w=800, h = 600;
 	bool move = false;
 
 	GLfloat i = PI/6, j = PI/6, speed_i = 20, speed_j = 20;
 void affichage ()
 {
-	GLbitfield masque = GL_COLOR_BUFFER_BIT;
-	glClear(masque);
-
-
+	glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT) ;
+	glMatrixMode( GL_MODELVIEW );
+	glLoadIdentity();
+	
+	gluLookAt(3,3,3,0,0,0,0,0,1);
+	glTranslatef(0.433,0.25,0);
+	glScalef(0.4,0.4,0.4);
+	glRotated(i, 0, 0, 1);
+	glRotated(j, 1, 0, 0);
+	
 	glBegin(GL_QUADS);
-	glColor3f(1.0, 0.0, 1.0);
-	glVertex3f(0.0, 0.0, 0.0);
-	//glColor3f(1.0, 1.0, 0.0);
-	glVertex3f(1.0, 0.0, 0.0);
-	//glColor3f(0.0, 1.0, 1.0);
-	glVertex3f(1.0, 1.0, 0.0);
-	//glColor3f(1.0, 0.0, 1.0);
-	glVertex3f(0.0, 1.0, 0.0);
-	//glEnd();
 
-	//glBegin(GL_QUADS);
-	glColor3f(0.0, 0.0, 1.0);
-	glVertex3f(0.0, 0.0, 1.0);
-	//glColor3f(1.0, 1.0, 0.0);
-	glVertex3f(1.0, 0.0, 1.0);
-	//glColor3f(0.0, 1.0, 1.0);
-	glVertex3f(1.0, 1.0, 1.0);
-	//glColor3f(1.0, 0.0, 1.0);
-	glVertex3f(0.0, 1.0, 1.0);
-	//glEnd();
+    glColor3ub(255,0,0); //face rouge
+		glVertex3d(1,1,1);
+		glVertex3d(1,1,-1);
+		glVertex3d(-1,1,-1);
+		glVertex3d(-1,1,1);
 
-	//glBegin(GL_POLYGON);
-	glColor3f(1.0, 0.0, 0.0);
-	glVertex3f(0.0, 0.0, 0.0);
-	//glColor3f(1.0, 1.0, 0.0);
-	glVertex3f(0.0, 1.0, 0.0);
-	//glColor3f(0.0, 1.0, 1.0);
-	glVertex3f(0.0, 1.0, 1.0);
-	//glColor3f(1.0, 0.0, 1.0);
-	glVertex3f(0.0, 0.0, 1.0);
-	//glEnd();
+    glColor3ub(0,255,0); //face verte
+		glVertex3d(1,-1,1);
+		glVertex3d(1,-1,-1);
+		glVertex3d(1,1,-1);
+		glVertex3d(1,1,1);
 
-	//glBegin(GL_POLYGON);
-	glColor3f(0.0, 1.0, 0.0);
-	glVertex3f(0.0, 0.0, 0.0);
-	//glColor3f(1.0, 1.0, 0.0);
-	glVertex3f(1.0, 0.0, 0.0);
-	//glColor3f(0.0, 1.0, 1.0);
-	glVertex3f(1.0, 0.0, 1.0);
-	//glColor3f(1.0, 0.0, 1.0);
-	glVertex3f(0.0, 0.0, 1.0);
-	//glEnd();
+    glColor3ub(0,0,255); //face bleue
+		glVertex3d(-1,-1,1);
+		glVertex3d(-1,-1,-1);
+		glVertex3d(1,-1,-1);
+		glVertex3d(1,-1,1);
 
-	//glBegin(GL_POLYGON);
-	glColor3f(0.0, 1.0, 1.0);
-	glVertex3f(1.0, 1.0, 0.0);
-	//glColor3f(1.0, 1.0, 0.0);
-	glVertex3f(1.0, 0.0, 0.0);
-	//glColor3f(0.0, 1.0, 1.0);
-	glVertex3f(1.0, 0.0, 1.0);
-	//glColor3f(1.0, 0.0, 1.0);
-	glVertex3f(1.0, 1.0, 1.0);
-	//glEnd();
+    glColor3ub(255,255,0); //face jaune
+		glVertex3d(-1,1,1);
+		glVertex3d(-1,1,-1);
+		glVertex3d(-1,-1,-1);
+		glVertex3d(-1,-1,1);
 
-	//glBegin(GL_POLYGON);
-	glColor3f(1.0, 1.0, 0.0);
-	glVertex3f(0.0, 1.0, 0.0);
-	//glColor3f(1.0, 1.0, 0.0);
-	glVertex3f(1.0, 1.0, 0.0);
-	//glColor3f(0.0, 1.0, 1.0);
-	glVertex3f(1.0, 1.0, 1.0);
-	//glColor3f(1.0, 0.0, 1.0);
-	glVertex3f(0.0, 1.0, 1.0);
-	glEnd();
-	glutSwapBuffers();
+    glColor3ub(0,255,255); //face cyan
+		glVertex3d(1,1,-1);
+		glVertex3d(1,-1,-1);
+		glVertex3d(-1,-1,-1);
+		glVertex3d(-1,1,-1);
+
+    glColor3ub(255,0,255); //face magenta
+		glVertex3d(1,-1,1);
+		glVertex3d(1,1,1);
+		glVertex3d(-1,1,1);
+		glVertex3d(-1,-1,1);
+
+    glEnd();
+    glutSwapBuffers();
+    glFlush();
+	
 }
 
 void reshape(int width, int height)
-{
-	w = width;
-	h = height;
-	if (x<y)
-		glViewport(0,(h-w)/2,w,w);
-	else
-		glViewport((w-h)/2,0,h,h);
-}
+{ 	 
+  	glViewport(0,0,width,height);
+  	glMatrixMode(GL_PROJECTION);
+  	glLoadIdentity();
+  	gluPerspective(45,float(width)/float(height),0.1,100); 
+  	glMatrixMode(GL_MODELVIEW);
+} 	 
 
 void clavier(unsigned char key, int x, int y)
 {
@@ -132,92 +114,10 @@ void mouse(int xp, int yp)
 	std::cout << x << "---" << y << std::endl;
 }
 
-GLfloat * CreateMatEch (GLfloat x, GLfloat y, GLfloat z)
-{
-	GLfloat M[] =
-{ x,   0.0, 0.0, 0.0,
-  0.0, y,   0.0, 0.0,
-  0.0, 0.0, z,   0.0,
-  0.0, 0.0, 0.0, 1.0};
-	return M;
-}
-
-GLfloat* CreateMatRotx (GLfloat angle)
-{
-	GLfloat M[] =
-{ 1.0,  0.0,         0.0,          0.0,
-  0.0,  cosf(angle), sinf(angle),  0.0,
-  0.0,  -sinf(angle),cosf(angle),  0.0,
-  0.0,  0.0,         0.0,          1.0};
-	return M;
-};
-
-GLfloat* CreateMatRotz (GLfloat angle)
-{
-	GLfloat M[] =
-{ cosf(angle),  sinf(angle),  0.0, 0.0,
-  -sinf(angle), cosf(angle),  0.0, 0.0,
-  0.0,          0.0,          1.0, 0.0,
-  0.0,          0.0,          0.0, 1.0};
-	return M;
-};
-
-GLfloat* CreateMatRoty (GLfloat angle)
-{
-	GLfloat M[] =
-{ cosf(angle),  0.0, -sinf(angle), 0.0,
-  0.0,          1.0, 0.0,          0.0,
-  sinf(angle),  0.0, cosf(angle),  0.0,
-  0.0,          0.0, 0.0,          1.0};
-	return M;
-};
-
-GLfloat* CreateMatTransVec (GLfloat Tx, GLfloat Ty, GLfloat Tz)
-{
-	GLfloat M[] =
-{ 1.0, 0.0, 0.0, 0.0,
-  0.0, 1.0, 0.0, 0.0,
-  0.0, 0.0, 1.0, 0.0,
-  Tx,  Ty,  Tz,  1.0};
-	return M;
-}
-
-GLfloat* CreateMatRotAxez (GLfloat angle)
-{
-	GLfloat M[] =
-{ (3 + cos (angle))/4,  				(sqrtf(3) * (-cos (angle) + 1))/4, 	(-2 * sin (angle))/4, 			0.0,
-  (sqrtf(3) * (-cos (angle) + 1))/4, 	(3 * cos(angle) +1)/4, 				(2* sqrtf(3) * sin(angle))/4, 	0.0,
-  (2 * sin (angle))/4, 					(-2 * sqrtf(3) * sin(angle))/4, 	(4 * cos(angle))/4, 			0.0,
-  0.0,  								0.0,  								0.0,  							1.0};
-	return M;
-}
-
-/*GLfloat* CreateMatTransAngz (GLfloat angle, )
-{
-	GLfloat M[] =
-{ 1.0, 0.0, 0.0, Tx,
-  0.0, 1.0, 0.0, Ty,
-  0.0, 0.0, 1.0, 0.0,
-  0.0, 0.0, 0.0, 1.0};
-}*/
-
 void idle ()
 {
-	glLoadIdentity();
-	glLoadMatrixf(CreateMatEch(0.4,0.4,0));
-	glMultMatrixf(CreateMatTransVec(0.433,0.25,0));
-	if (move)
-	{
-//	gluLookAt(	0.f, 0.f, 0.f,
-//				0.f, 0.f,    0.f,
-//				0.f, 0.f,  1.f);
 	i+=0.005*speed_i/20;
 	j+=0.005*speed_j/20;
-	//glMultMatrixf(CreateMatRotz(j));
-
-	//glMultMatrixf();
-	}
-	glMultMatrixf(CreateMatRotAxez(i));
 	glutPostRedisplay();
 }
 } //namespace
@@ -227,13 +127,18 @@ int main (int argc, char * argv[])
 
 	glutInit(&argc, argv);
 	glutInitDisplayMode(GLUT_DEPTH | GLUT_DOUBLE | GLUT_RGB);
-	glutInitWindowPosition(200,200);
-	glutInitWindowSize(250,250);
+	glutInitWindowPosition(50,50);
+	glutInitWindowSize(w,h);
 	glutCreateWindow("ogl1");
 	glHint(GL_POLYGON_SMOOTH_HINT, GL_NICEST);
-
+	glEnable(GL_DEPTH_TEST);
 	glClearColor(0.5,0.5,0.5,1.0);
-	glPointSize(2.0);
+	glPointSize(1.0);
+
+	glMatrixMode( GL_PROJECTION );
+	glLoadIdentity();
+	gluPerspective(10,(double)w/h,0,100);
+	glMatrixMode( GL_MODELVIEW );
 
 	glutDisplayFunc(affichage);
 	glutIdleFunc(idle);
---
+++ transformations.h
@@ -0,0 +1,68 @@
+GLfloat * CreateMatEch (GLfloat x, GLfloat y, GLfloat z)
+{
+	GLfloat M[] =
+{ x,   0.0, 0.0, 0.0,
+  0.0, y,   0.0, 0.0,
+  0.0, 0.0, z,   0.0,
+  0.0, 0.0, 0.0, 1.0};
+	return M;
+}
+
+GLfloat* CreateMatRotx (GLfloat angle)
+{
+	GLfloat M[] =
+{ 1.0,  0.0,         0.0,          0.0,
+  0.0,  cosf(angle), sinf(angle),  0.0,
+  0.0,  -sinf(angle),cosf(angle),  0.0,
+  0.0,  0.0,         0.0,          1.0};
+	return M;
+};
+
+GLfloat* CreateMatRotz (GLfloat angle)
+{
+	GLfloat M[] =
+{ cosf(angle),  sinf(angle),  0.0, 0.0,
+  -sinf(angle), cosf(angle),  0.0, 0.0,
+  0.0,          0.0,          1.0, 0.0,
+  0.0,          0.0,          0.0, 1.0};
+	return M;
+};
+
+GLfloat* CreateMatRoty (GLfloat angle)
+{
+	GLfloat M[] =
+{ cosf(angle),  0.0, -sinf(angle), 0.0,
+  0.0,          1.0, 0.0,          0.0,
+  sinf(angle),  0.0, cosf(angle),  0.0,
+  0.0,          0.0, 0.0,          1.0};
+	return M;
+};
+
+GLfloat* CreateMatTransVec (GLfloat Tx, GLfloat Ty, GLfloat Tz)
+{
+	GLfloat M[] =
+{ 1.0, 0.0, 0.0, 0.0,
+  0.0, 1.0, 0.0, 0.0,
+  0.0, 0.0, 1.0, 0.0,
+  Tx,  Ty,  Tz,  1.0};
+	return M;
+}
+
+GLfloat* CreateMatRotAxez (GLfloat angle)
+{
+	GLfloat M[] =
+{ (3 + cos (angle))/4,  				(sqrtf(3) * (-cos (angle) + 1))/4, 	(-2 * sin (angle))/4, 			0.0,
+  (sqrtf(3) * (-cos (angle) + 1))/4, 	(3 * cos(angle) +1)/4, 				(2* sqrtf(3) * sin(angle))/4, 	0.0,
+  (2 * sin (angle))/4, 					(-2 * sqrtf(3) * sin(angle))/4, 	(4 * cos(angle))/4, 			0.0,
+  0.0,  								0.0,  								0.0,  							1.0};
+	return M;
+}
+
+/*GLfloat* CreateMatTransAngz (GLfloat angle, )
+{
+	GLfloat M[] =
+{ 1.0, 0.0, 0.0, Tx,
+  0.0, 1.0, 0.0, Ty,
+  0.0, 0.0, 1.0, 0.0,
+  0.0, 0.0, 0.0, 1.0};
+}*/
